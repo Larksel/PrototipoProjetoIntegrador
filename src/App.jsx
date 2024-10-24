ef283f1a0107654032975237890533a64e50b9c6
Larksel <lemuelgames@outlook.com>
2024-10-24 02:05:53 -0300

    :sparkles: feat: Controle do tamanho da fonte

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
-import { useContext } from "react"
-import { ThemeContext } from "./context/ThemeContext"
+import { useContext } from 'react';
+import { ThemeContext } from './context/ThemeContext';
 
 export default function App() {
-  const { toggleTheme } = useContext(ThemeContext)
-
-  const lightTheme = () => {
-    toggleTheme('light');
-  }
-
-  const darkTheme = () => {
-    toggleTheme('dark')
-  }
-
-  const highContrast = () => {
-    toggleTheme('high-contrast')
-  }
+  const { fontSize, changeFontSize, toggleTheme } = useContext(ThemeContext);
 
   return (
     <div className='flex flex-col text-text bg-background h-screen p-8'>
       <div className='flex flex-col h-full rounded-2xl overflow-hidden'>
-        <div className='text-2xl text-center bg-natural'>
+        <div className='text-2xl text-center bg-natural flex flex-col'>
           <h3>text-text bg-natural</h3>
-          <button onClick={lightTheme} className="underline">Toggle Light Theme</button>
+          <button onClick={() => toggleTheme('light')} className='underline'>
+            Toggle Light Theme
+          </button>
+          <button onClick={() => changeFontSize(fontSize - 2)} className='underline'>
+            Fonte menor
+          </button>
+          <button onClick={() => changeFontSize(fontSize + 2)} className='underline'>
+            Fonte maior
+          </button>
         </div>
         <div className='flex-grow bg-primary'>
           <h3>bg-primary</h3>
-          <button onClick={darkTheme} className="underline">Toggle Dark Theme</button>
+          <button onClick={() => toggleTheme('dark')} className='underline'>
+            Toggle Dark Theme
+          </button>
         </div>
         <div className='flex-grow bg-secondary'>
           <h3>bg-secondary</h3>
-          <button onClick={highContrast} className="underline">Toggle High Contrast</button>
+          <button
+            onClick={() => toggleTheme('high-contrast')}
+            className='underline'
+          >
+            Toggle High Contrast
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,24 +1,37 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from 'react';
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('light');
-  
+  const [fontSize, setFontSize] = useState(16); // Tamanho da fonte em pixels
+
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme') || 'light';
+    const savedFontSize = localStorage.getItem('fontSize') || 16;
     setTheme(savedTheme);
+    setFontSize(savedFontSize);
     document.documentElement.setAttribute('data-theme', savedTheme);
-  }, [])
+    document.documentElement.setAttribute('data-fontSize', savedFontSize);
+  }, []);
 
   const toggleTheme = (newTheme) => {
     setTheme(newTheme);
     localStorage.setItem('theme', newTheme);
     document.documentElement.setAttribute('data-theme', newTheme);
-  }
+  };
+
+  const changeFontSize = (newFontSize) => {
+    if (newFontSize < 0) newFontSize = 0; // Impede número negativo
+    setFontSize(newFontSize);
+    localStorage.setItem('fontSize', newFontSize);
+    document.documentElement.setAttribute('data-fontSize', newFontSize);
+  };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider
+      value={{ theme, fontSize, changeFontSize, toggleTheme }}
+    >
       {children}
     </ThemeContext.Provider>
   );
